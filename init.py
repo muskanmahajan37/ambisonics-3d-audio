b98638fcc749dc6ebadaf8668f6aa99336714860
shivasiddharth <ushivasiddharth@gmail.com>
2020-04-28 15:05:22 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,14 +1,34 @@
+#!/usr/bin/env python
+
 from glob import glob
 from pydub import AudioSegment
 from pydub.generators import WhiteNoise
 from math import *
 from random import *
+import argparse
+import subprocess
 import sys
 
-if len(sys.argv) > 2:
-	AudioSegment.converter = sys.argv[1] #ffmpeg installation exe dir path
-	AudioSegment.ffmpeg = sys.argv[1] #ffmpeg installation exe dir path
-	AudioSegment.ffprobe = sys.argv[2] #ffprobe installation exe dir path
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+
+parser.add_argument('-i', type=str,
+                    metavar='Source audio path', required=True,
+                    help='Path to source file.')
+parser.add_argument('-o', type=str,
+                    metavar='Modified audio path', required=True,
+                    help='Path to modified file.')
+
+args = parser.parse_args()
+
+if not args.i and not i:
+    raise Exception('Missing -i Input option')
+if not args.o and not o:
+    raise Exception('Missing -o Output option')
+
+# if len(sys.argv) > 2:
+# 	AudioSegment.converter = sys.argv[1] #ffmpeg installation exe dir path
+# 	AudioSegment.ffmpeg = sys.argv[1] #ffmpeg installation exe dir path
+# 	AudioSegment.ffprobe = sys.argv[2] #ffprobe installation exe dir path
 
 def calc_pan(index):
 	return cos(radians(index))
@@ -17,14 +37,14 @@ def calc_pan(index):
 
 #first_song = playlist_songs.pop(0)
 interval = 0.2 * 1000 # sec
-song = AudioSegment.from_mp3('mp3/everlong.mp3')
+song = AudioSegment.from_mp3(args.i)
 song_inverted = song.invert_phase()
 song.overlay(song_inverted)
 
 splitted_song = splitted_song_inverted = []
 song_start_point = 0
 
-print("split song in part")
+print("Splitting track " + args.i + " in parts......")
 while song_start_point+interval < len(song):
     splitted_song.append(song[song_start_point:song_start_point+interval])
     song_start_point += interval
@@ -32,7 +52,7 @@ while song_start_point+interval < len(song):
 if song_start_point < len(song):
     splitted_song.append(song[song_start_point:])
 
-print("end splitting")
+print("End of splitting......")
 print("total pieces: " + str(len(splitted_song)))
 
 ambisonics_song = splitted_song.pop(0)
@@ -42,8 +62,9 @@ for piece in splitted_song:
     piece = piece.pan(calc_pan(pan_index))
     ambisonics_song = ambisonics_song.append(piece, crossfade=interval/50)
 
-
 # lets save it!
-out_f = open("compiled/everlong.mp3", 'wb')
+out_f = open("8d.mp3", 'wb')
 
 ambisonics_song.export(out_f, format='mp3')
+
+subprocess.call("ffmpeg -i 8d.mp3 -codec:a libmp3lame -b:a 320k " + args.o, shell=True)
