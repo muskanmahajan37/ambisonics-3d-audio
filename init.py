85deddb1d109514db82d83950dce06d7fdf4d5bf
shivasiddharth <ushivasiddharth@gmail.com>
2020-04-28 13:48:16 +0530

    Update init.py

--- init.py
+++ init.py
@@ -24,7 +24,7 @@ song.overlay(song_inverted)
 splitted_song = splitted_song_inverted = []
 song_start_point = 0
 
-print "split song in part"
+print("split song in part")
 while song_start_point+interval < len(song):
     splitted_song.append(song[song_start_point:song_start_point+interval])
     song_start_point += interval
@@ -32,8 +32,8 @@ while song_start_point+interval < len(song):
 if song_start_point < len(song):
     splitted_song.append(song[song_start_point:])
 
-print "end splitting"
-print "total pieces: " + str(len(splitted_song))
+print("end splitting")
+print("total pieces: " + str(len(splitted_song)))
 
 ambisonics_song = splitted_song.pop(0)
 pan_index = 0
